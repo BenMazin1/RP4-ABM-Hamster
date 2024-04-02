a2de14fc33509d202ac2ba1953857b3440746ccc
BenMazin <binyaminmazin@gmail.com>
2024-04-02 16:09:16 -0400

    clean up files and add SD to Part 2 analysis

--- netlogo analysisP2.py
+++ netlogo analysisP2.py
@@ -83,3 +83,20 @@ plothist(results['ChemoTotalDead'], 'Total Dead with Doxorubicin Treatment', 'To
 plothist(results['ChemoPercentInfect'], 'Infection with Doxorubicin Treatment', 'Most Infected at One Time', 'Frequency', save_path + 'ChemoMostInfectHist.png')
 plothist(results['NoChemoTotalDead'], 'Total Dead WITHOUT Doxorubicin Treatment', 'Total Dead', 'Frequency', save_path + 'NoChemoTotalDeadHist.png')
 plothist(results['NoChemoPercentInfect'], 'Infection WITHOUT Doxorubicin Treatment', 'Most Infected at One Time', 'Frequency', save_path + 'NoChemoMostInfectHist.png')
+
+data = results
+
+# calculate the standard deviation
+std_dev1 = np.std(data['ChemoTotalDead'].dropna())
+std_dev2 = np.std(data['NoChemoTotalDead'].dropna())
+std_dev3 = np.std(data['ChemoPercentInfect'].dropna())
+std_dev4 = np.std(data['NoChemoPercentInfect'].dropna())
+
+# create a DataFrame to store the results
+result = pd.DataFrame({'ChemoTotalDead_std_dev': [std_dev1],
+                       'NoChemoTotalDead_std_dev': [std_dev2],
+                       'ChemoPercentInfect_std_dev': [std_dev3],
+                       'NoChemoPercentInfect_std_dev': [std_dev4]})
+
+# save the DataFrame as a CSV file
+result.to_csv('/Users/benmazin/Code Dev/RP 4/netlogo results/std_dev_results.csv', index=False)
